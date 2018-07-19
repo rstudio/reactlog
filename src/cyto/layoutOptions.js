7ace7b718a2213515fe0bb15f1f9e8282a120c82
Barret Schloerke <schloerke@gmail.com>
2018-07-19 11:41:53 -0400

    by default do not fit the layout into the space.  Only on major graph layout updates

--- src/cyto/layoutOptions.js
+++ src/cyto/layoutOptions.js
@@ -8,6 +8,10 @@ import type { CytoscapeElement, CytoscapeLibrary } from "./cytoFlowType";
 (cytoscape: CytoscapeLibrary).use(dagre);
 
 let layoutOptions = {
+  // whether to fit to viewport
+  //   do not want to fit to viewport as user may have zoomed/panned
+  fit: false,
+
   name: "dagre",
   rankDir: "LR", // 'TB' for top to bottom flow, 'LR' for left to right,
   rankSep: 150, // the separation between node columns
